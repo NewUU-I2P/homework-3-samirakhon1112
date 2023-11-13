b06ecf95fe4e80bb89d069594a88116ba74f80ed
Samirakhon1112 <s.jamolxonova>
2023-11-13 08:56:18 +0500

    problem_3

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -7,7 +7,7 @@ std::string problemSolution3(float height, char S) {
         if (height < 1.70) {
             cout << "short" << endl;
         } if else (height >= 1.70 && height == 1.85) {
-            cout << "normal" << enf=dl;
+            cout << "normal" << endl;
         } else {
             cout << "tall" << endl;
         }
@@ -15,10 +15,12 @@ std::string problemSolution3(float height, char S) {
         if (height < 1.60) {
             cout << "short" << endl;
         } if else (height >= 1.60 && height == 1.75) {
-            cout << "normal" << enf=dl;
+            cout << "normal" << endl;
         } else {
             cout << "tall" << endl;
         }
+    } else {
+        cout << "undefined gender"
     }
     // use return to return your result
     // make use of control flow statements
