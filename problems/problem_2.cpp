1d2be41c6d173efa8e5ed47ab75aa0e8721d3412
Samirakhon1112 <s.jamolxonova>
2023-11-13 08:44:32 +0500

    problem_2

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,14 +1,6 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
-    cout << "Enter first price: " << endl;
-    cin >> price1;
-    cout << "Enter second price: " << endl;
-    cin >> price2;
-    cout << "Enter third price: " << endl;
-    cin >> price3;
-    double highestPrice = (price1 > price2) ? ((price1 > price3) ? price1 : price3) : ((price2 > price3) ? price2 : price3);
-    cout << "The highest price is " << highestPrice << endl;
-}
+
     return result;
 }
