013d967fa7e288081244b5541ea0c3aee965b0ee
Samirakhon1112 <s.jamolxonova>
2023-11-14 22:04:51 +0500

    Problem_4

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,21 +3,31 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
-    std::istringstream iss(macAdress);
+
+    std::istringstream iss(macAddress);
     std::string octet;
+
     getline(iss,octet,':');
     int firstOctet;
+
     std::stringstream(octet) >> std::hex >> firstOctet;
     if (firstOctet % 2 == 0) {
+
         return "Unicast";
+
     }
-    else if (firstOctet % 2 != 0 && firstOctet == 255) {
-        return  "Multicast";
-    }
-    else if (firstOctet == 255) {
-        return "Broadcast";
+    else if (firstOctet %2 != 0 and firstOctet != 255){
+
+        return "Multicast";
     }
 
+    else if (firstOctet == 255) {
+        return "Broadcast";
+
+    }
     // make use of control flow statements
+
     // return result;
 }
+
+
