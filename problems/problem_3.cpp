dc10407c45b3334c741a3de70fa6f78ad721dc20
Samirakhon1112 <s.jamolxonova>
2023-11-14 22:07:23 +0500

    Problem_4

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -3,27 +3,39 @@
 std::string problemSolution3(float height, char S) {
     // write your code here
     std::string heightDescription;
+
     if (S == 'M') {
         if (height < 1.7) {
+
             heightDescription = "Short";
-        } if else (height >= 1.7 && height < 1.85)  {
+        } else if (height >= 1.7 && height < 1.85) {
+
             heightDescription = "Normal";
         } else {
+
             heightDescription = "Tall";
         }
-    } if else (S == 'F') {
+
+    } else if (S == 'F') {
         if (height < 1.6) {
+
             heightDescription = "Short";
-        } if else (height >= 1.6 && height < 1.75) {
+        } else if (height >= 1.6 && height < 1.75) {
+
             heightDescription = "Normal";
         } else {
+
             heightDescription = "Tall";
+
         }
     } else {
-        heightDescription = "Undefined gender";
+
+        heightDescription = "Invalid sex";
     }
+
 
     return heightDescription;
     // use return to return your result
+
     // make use of control flow statements
 }
